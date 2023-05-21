597725af63868c8c6b4136deac6553463ab9eea6
ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>
2023-05-21 14:46:58 -0500

    feat: adds pokemon pagination & modal details

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff4e52",
+        "custom-gray-50": "#f7f8fc",
+        "custom-gray-100": "#f0f1f6",
+      },
+      fontFamily: {
+        lato: ["Lato"],
+      },
+    },
   },
   plugins: [],
 };
